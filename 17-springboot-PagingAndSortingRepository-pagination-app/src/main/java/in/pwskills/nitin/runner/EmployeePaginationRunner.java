4529f0570d4b49910f5b4ea2090fb16c3bcddac1
ayush9599sharma <dsayushkapil@gmail.com>
2026-02-19 22:47:06 +0530

    adding pagination logic to get data in pages

--- 17-springboot-PagingAndSortingRepository-pagination-app/src/main/java/in/pwskills/nitin/runner/EmployeePaginationRunner.java
+++ 17-springboot-PagingAndSortingRepository-pagination-app/src/main/java/in/pwskills/nitin/runner/EmployeePaginationRunner.java
@@ -2,28 +2,35 @@ package in.pwskills.nitin.runner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Component;
 
 import in.pwskills.nitin.dao.IEmployeePagingAndSortingRepository;
+import in.pwskills.nitin.entity.Employee;
 
 @Component
-public class EmployeeSortingRunner implements CommandLineRunner {
+public class EmployeePaginationRunner implements CommandLineRunner {
 	
 	@Autowired
 	private IEmployeePagingAndSortingRepository repo;
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repo.findAll(Sort.by("empSal")).forEach(System.out::println);
+		repo.findAll(PageRequest.of(0, 4)).forEach(System.out::println);
+		repo.findAll(PageRequest.of(1, 2)).forEach(System.out::println);
+		repo.findAll(PageRequest.of(2, 4)).forEach(System.out::println);
 		
-		repo.findAll(Sort.by(Direction.DESC, "empSal")).forEach(System.out::println);
+		Page<Employee> page = repo.findAll(PageRequest.of(0, 4));
+		System.out.println(page.getSize());
+		System.out.println(page.getTotalPages());
 		
-		repo.findAll(Sort.by(Direction.DESC,"empSal","empDept")).forEach(System.out::println);
 		
-		repo.findAll(Sort.by(Order.asc("empSal"),Order.desc("empDept"))).forEach(System.out::println);
+		
+		
 	}
 
 }
